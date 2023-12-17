bc8c48888f07f81b000f2fb3ef5ee77499d8333f
sebastian <sebastian.narvaez@johannaortiz.com>
2023-12-17 00:04:37 -0500

    Inicio del proyecto

---
+++ src/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args){
+        int speed = 36;
+
+        int salary = 2600;
+
+        String employeeName = "Raul reyes";
+        System.out.println(speed);
+        System.out.println(salary);
+        System.out.println(employeeName);
+    }
+}
